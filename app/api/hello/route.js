c10323c15265f2a4cbef8bacc34d81cc32124062
Su Shwe Yaung <sushweyaung8989@gmail.com>
2026-01-30 00:05:41 +0700

    Connect support for React (CORS)

--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -1,3 +1,9 @@
 export async function GET() {
-  return Response.json({ message: "hello world" });
+  return new Response(JSON.stringify({ message: "hello world" }), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+  });
 }
